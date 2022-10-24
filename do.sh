6aec7bb3188645243c1a6cdc1cfe9d0a7450f122
jbfink <john.fink@gmail.com>
2022-10-24 19:21:05 +0000

    Reworked do.sh to put all output in a single file which is timestamped.

--- do.sh
+++ do.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-# Note: for the moment this file needs to be executed in the same directory as your sample files, so you will have to move it if you're seeing it in the top directory. Also, you will want to change the directory path (from /home/jbfink/DeepSeeNet/ to wherever you have this installed and where your samples are. Sorry about that, I'm terrible at scripting.
-
+# Note: for the moment this file needs to be executed in the same directory as your sample files. Also, you will want to change the directory path (from /home/jbfink/DeepSeeNet/ to wherever you have this installed and where your samples are. Sorry about that, I'm terrible at scripting.
 
 export PYTHONPATH=/home/jbfink/DeepSeeNet:$PYTHONPATH
 list=`ls -1 retinal_BL_left_*.jpg | sed -e "s/retinal_BL_left_//;s/\.jpg//"`
 
+date=`date +%Y%m%d%H%M`
+
 echo $list
 for j in $list; do
 	if [ -f retinal_BL_right_$j.jpg ] ; then
-	python3 /home/jbfink/DeepSeeNet/examples/predict_simplified_score.py /home/jbfink/DeepSeeNet/rumaisa-sample/retinal_BL_left_$j.jpg /home/jbfink/DeepSeeNet/rumaisa-sample/retinal_BL_right_$j.jpg 2>&1 | tail -4 > /tmp/output_$j.txt
+	echo "$j" >> /tmp/output_$date.txt
+	echo "---------" >> /tmp/output_$date.txt
+	python3 /home/jbfink/DeepSeeNet/examples/predict_simplified_score.py /home/jbfink/DeepSeeNet/rumaisa-sample/retinal_BL_left_$j.jpg /home/jbfink/DeepSeeNet/rumaisa-sample/retinal_BL_right_$j.jpg 2>&1 | tail -4 >> /tmp/output_$date.txt
+	echo "---------" >> /tmp/output_$date.txt
+	echo "" >> /tmp/output_$date.txt
 	fi
 done
